c4db719333773225513eb8e97dc8947ab8cc2a0f
cmillecan <cmillecan@gmail.com>
2020-07-24 00:54:14 +0000

    add file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -19,7 +19,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	r = binary_tree_height(tree->right);
 	if (l != r)
 		return (0);
-	if (l && r)
+	if (binary_tree_is_perfect(tree->left)
+		&& binary_tree_is_perfect(tree->right))
 		return (1);
 	else
 		return (0);
