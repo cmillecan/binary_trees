13175d00cb8ab695ef2f19232711cbea1e46e34d
cmillecan <cmillecan@gmail.com>
2020-07-24 00:15:19 +0000

    add file

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -33,11 +33,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (0);
 
-	left_h = binary_tree_height(tree->left);
-	right_h = binary_tree_height(tree->right);
+	left_h = 1 + binary_tree_height(tree->left);
+	right_h = 1 + binary_tree_height(tree->right);
 
 	if (left_h > right_h)
-		return (left_h + 1);
+		return (left_h);
 	else
-		return (right_h + 1);
+		return (right_h);
 }
